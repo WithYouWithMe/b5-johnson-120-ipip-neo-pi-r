e448a1c68f9e2471ac0b8539364090fe5cf189b6
Oscar Duretto <oscar@withyouwithme.com>
2020-02-12 16:52:29 +1100

    converted index.js to es5

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-const { knuthShuffle } = require('knuth-shuffle')
-const languages = require('./data/languages.json')
+"use strict";
 
-module.exports.getItems = (lang = 'en', shuffle = false) => {
-  let choices, questions
+var _require = require('knuth-shuffle'),
+knuthShuffle = _require.knuthShuffle;
+var languages = require('./data/languages.json')
+
+export function getItems(lang, shuffle) {
+  var lang = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'en';
+  var shuffle = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
+  var choices, questions
   try {
-    questions = require(`./data/${lang}/questions.json`)
-    choices = require(`./data/${lang}/choices`)
+    questions = require("./data/".concat(lang, "/questions.json"))
+    choices = require("./data/".concat(lang, "/choices"))
   } catch (error) {
     throw new Error('Inventory not found. Try another language input.')
   }
 
-  const inventory = shuffle === true ? knuthShuffle(questions) : questions
-  return inventory.map((question, i) => Object.assign(question, { num: ++i, choices: choices[question.keyed] }))
+  var inventory = shuffle === true ? knuthShuffle(questions) : questions
+  return inventory.map(function (question, i) {
+    return Object.assign(question, { num: ++i, choices: choices[question.keyed] });
+  })
 }
 
-module.exports.getInfo = () => (
-  {
-    name: `Johnson's IPIP NEO-PI-R`,
+export function getInfo() {
+  return {
+    name: "Johnson's IPIP NEO-PI-R",
     id: 'johnson-120-ipip-neo-pi-r',
     shortId: 'b5-120',
     time: 10,
     questions: 120,
     note: 'Recommended',
-    languages
+    languages: languages
   }
-)
+}
